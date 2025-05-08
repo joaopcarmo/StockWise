c35441d0fc391b79f2b5369242b659d9b953bbde
João Pedro <joaocarmolp@gmail.com>
2025-05-08 16:33:30 -0300

    testando bd

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuario (
+                         id UUID PRIMARY KEY,
+                         nome VARCHAR(100) NOT NULL,
+                         telefone VARCHAR(20) NOT NULL UNIQUE,
+                         endereco VARCHAR(255) NOT NULL,
+                         email VARCHAR(100) NOT NULL UNIQUE,
+                         senha VARCHAR(255) NOT NULL
+);
